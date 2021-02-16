c99a8d5dd77a5c2dccdd442a593314b5ec4019dc
Yli-Hemminki <Juha.Yli-Hemminki@seamk.fi>
2021-02-16 11:03:47 +0200

    Add project files.

---
+++ ConsoleApp10/Auto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    public class Auto : IComparable<Auto>
+    {
+        private String merkki;
+        private int vuosimalli;
+
+        public Auto(String merkki, int vuosimalli)
+        {
+            this.merkki = merkki;
+            this.vuosimalli = vuosimalli;
+        }
+
+        public void TulostaTiedot()
+        {
+            Console.WriteLine("Merkki: " + merkki + "\tVuosimalli: " + vuosimalli);
+        }
+
+        public int CompareTo(Auto vertaileAuto)
+        {
+            if (vertaileAuto == null)
+                return 1;
+            else
+                return this.vuosimalli.CompareTo(vertaileAuto.vuosimalli);
+        }
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static List<Auto> lista;
+
+        static void Main(string[] args)
+        {
+            lista = new List<Auto>();
+
+            lista.Add(new Auto("Austin", 1969));
+            lista.Add(new Auto("Ford", 1938));
+            lista.Add(new Auto("Honda", 2017));
+            lista.Add(new Auto("Mazda", 1990));
+
+            foreach (Auto a in lista) a.TulostaTiedot();
+
+            Console.WriteLine();
+
+            lista.Sort();
+
+            foreach (Auto a in lista) a.TulostaTiedot();
+
+            Console.ReadLine();
+        }
+    }
+}
